4daa5cbfecf640381b376d9f5c56cf285c45f482
royossai <royossai@yahoo.com>
2021-04-28 02:23:32 +0200

    patch

--- src/move.js
+++ src/move.js
@@ -117,7 +117,7 @@ const move = async function (options, item, bar, hold) {
 
             if (matches && realFiles.length === 1) {
                 bar.show('moving ' + realFile);
-                console.log(11, item + realFile, hold , realFile, realFile.replace(/\[.*?]\s*|-|\(.*?\)\s*|/g, ''))
+                console.log(11, item + realFile, hold , realFile, realFile.replace(/\s*/, '.'))//.replace(/\[.*?]\s*|-|\(.*?\)\s*|/g, ''))
                 //await renameFile(item + realFile, hold + realFile.replace(/\[.*?]\s*|-|\(.*?\)\s*|/g, '').replace(/\s*/, '.'));
                 return true;
 
